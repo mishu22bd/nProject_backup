73a06c28f8dedf1d5164ea62319ca46cfdb43f06
Sayem <sayem@sayem-Lenovo-IdeaPad-U410.(none)>
2015-08-13 18:37:45 +0600

    This is a plugin for workload of an employee

--- nProject/db/schema.rb
+++ nProject/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -29,6 +28,12 @@ ActiveRecord::Schema.define(:version => 20150220141744) do
   end
 
   add_index "addresses", ["addressable_id", "addressable_type"], :name => "index_addresses_on_addressable_id_and_addressable_type"
+
+  create_table "arns", :force => true do |t|
+    t.string  "question"
+    t.integer "yes",      :default => 0
+    t.integer "no",       :default => 0
+  end
 
   create_table "attachments", :force => true do |t|
     t.integer  "container_id"
